325615bb9d2a01339dcab7e50c120c4d0d80a523
RafaelSandrini <68172553+RafaelSandrini@users.noreply.github.com>
2022-01-11 19:49:12 -0500

    Added winston logger to handle errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const winston = require('winston');
 const app = express();
 require('dotenv').config();
 const restaurantsRoute = require('./routes/restaurants');
@@ -10,17 +11,39 @@ const PORT = process.env.PORT || 3000
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+
+// create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console({
+            format:winston.format.combine(
+                winston.format.colorize({all:true})
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log' ,level:'error' })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: 'exceptions.log'})
+    ]
+});
+
+
 // routes
 app.use('/api/restaurants',restaurantsRoute);
 
 
+
 // connect to mongodb atlas
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}).then(() => {
-    console.log("Connected to mongodb atlas");
+    logger.log("info","connected to mongoDb atlas");
+    // console.log("Connected to mongodb atlas");
+}).catch((error) => {
+    logger.error(error.message);
 });
 
 
 // start server
 app.listen(PORT, () => {
-    console.log("Server started at ", PORT);
-})
+    logger.info(`Server started at PORT ${PORT}`);
+});
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -22,4 +22,52 @@ router.post('/', async (req, res) => {
 
 });
 
+// get all restaurants
+router.get("/",(req,res) => {
+    Restaurants.find()
+        .then((restaurant) => res.send(restaurant))
+        .catch((error) => {
+            res.status(500).send("Something went wrong");
+    });
+});
+
+// get restaurant by ID
+router.get("/:restaurantId", async (req,res) => {
+    const restaurant = await Restaurants.findById(req.params.restaurantId);
+    if (!restaurant) return res.status(404).send("Restaurant not found");
+    res.send(restaurant);
+    //     .then((restaurant) => {
+    //         if(restaurant) return res.send(restaurant);
+    //         // added the return to avoid the error=> code: 'ERR_HTTP_HEADERS_SENT' after try to get some restaurant
+    //         res.status(404).send("Restaurant not found");
+    //     })
+    //     .catch((error) => {
+    //         res.status(500).send(error.message);
+    //     });
+});
+
+// update restaurant based on ID
+router.put("/:restaurantId", async(req,res) => {
+    const updatedRestaurant = await Restaurants.findByIdAndUpdate(
+        req.params.restaurantId, 
+        {
+            name:req.body.restaurantName,
+            address:req.body.restaurantAddress,
+            cuisine:req.body.restaurantCuisine
+        },
+        {new:true}
+        );
+
+        if(!updatedRestaurant) return res.status(404).send("Book not found");
+        res.send(updatedRestaurant);
+});
+
+// delete a book based on ID
+router.delete("/:restaurantId", async (req,res) => {
+    const restaurant = await Restaurants.findByIdAndRemove(req.params.restaurantId);
+    if(!restaurant) return res.status(404).send("Restaurant with Id not found");
+    res.send(restaurant);
+});
+
+
 module.exports = router;
